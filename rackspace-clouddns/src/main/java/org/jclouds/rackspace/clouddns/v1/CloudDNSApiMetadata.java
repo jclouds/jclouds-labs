ea9b52e9441c62569a3db5bd43df6e6f50f01d8f
adriancole <adrian.f.cole@gmail.com>
2013-04-10 08:58:44 -0700

    adjust Mapped -> SyncToAsync

--- rackspace-clouddns/src/main/java/org/jclouds/rackspace/clouddns/v1/CloudDNSApiMetadata.java
+++ rackspace-clouddns/src/main/java/org/jclouds/rackspace/clouddns/v1/CloudDNSApiMetadata.java
@@ -30,7 +30,7 @@ import org.jclouds.rackspace.clouddns.v1.config.CloudDNSRestClientModule;
 import org.jclouds.rackspace.cloudidentity.v2_0.ServiceType;
 import org.jclouds.rackspace.cloudidentity.v2_0.config.CloudIdentityAuthenticationModule;
 import org.jclouds.rackspace.cloudidentity.v2_0.config.CloudIdentityCredentialTypes;
-import org.jclouds.rackspace.cloudidentity.v2_0.config.MappedCloudIdentityAuthenticationApiModule;
+import org.jclouds.rackspace.cloudidentity.v2_0.config.SyncToAsyncCloudIdentityAuthenticationApiModule;
 import org.jclouds.rest.RestContext;
 import org.jclouds.rest.internal.BaseRestApiMetadata;
 
@@ -83,7 +83,7 @@ public class CloudDNSApiMetadata extends BaseRestApiMetadata {
                .defaultProperties(CloudDNSApiMetadata.defaultProperties())
                .defaultModules(
                      ImmutableSet.<Class<? extends Module>> builder()
-                        .add(MappedCloudIdentityAuthenticationApiModule.class)
+                        .add(SyncToAsyncCloudIdentityAuthenticationApiModule.class)
                         .add(CloudIdentityAuthenticationModule.class)
                         .add(ProviderModule.class)
                         .add(CloudDNSRestClientModule.class)
