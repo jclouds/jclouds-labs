14207a33cf4c8a1d9a32615bdf0c762b7406d910
Adrian Cole <acole@twitter.com>
2014-10-16 19:01:17 -0700

    Removed unused JAXB annotations.

--- azurecompute/src/main/java/org/jclouds/azurecompute/domain/Protocol.java
+++ azurecompute/src/main/java/org/jclouds/azurecompute/domain/Protocol.java
@@ -16,16 +16,8 @@
  */
 package org.jclouds.azurecompute.domain;
 
-import javax.xml.bind.annotation.XmlEnum;
-import javax.xml.bind.annotation.XmlEnumValue;
-import javax.xml.bind.annotation.XmlType;
-
-@XmlType(name = "Protocol") @XmlEnum
 public enum Protocol {
-   @XmlEnumValue("tcp")
    TCP,
-   @XmlEnumValue("http")
    HTTP,
-   @XmlEnumValue("udp")
    UDP
 }
