036c68f2de1296585dcccb1e29b301cdf78f7607
Ignasi Barrera <nacx@apache.org>
2018-01-04 01:44:14 +0100

    JCLOUDS-1362: Proper password generation with custom constraints for each cloud

--- profitbricks-rest/src/main/java/org/apache/jclouds/profitbricks/rest/domain/Volume.java
+++ profitbricks-rest/src/main/java/org/apache/jclouds/profitbricks/rest/domain/Volume.java
@@ -20,9 +20,10 @@ import com.google.auto.value.AutoValue;
 import com.google.common.base.Enums;
 import static com.google.common.base.Preconditions.checkArgument;
 import java.util.Set;
+
+import org.apache.jclouds.profitbricks.rest.util.Preconditions;
 import org.jclouds.javax.annotation.Nullable;
 import org.jclouds.json.SerializedNames;
-import org.jclouds.util.Passwords;
 
 @AutoValue
 public abstract class Volume extends Trackable {
@@ -197,7 +198,7 @@ public abstract class Volume extends Trackable {
                CreatePayload payload = autoBuild();
 
                if (payload.imagePassword() != null) {
-                  checkArgument(Passwords.isValidPassword(payload.imagePassword()), "Password's format is not valid");
+                  Preconditions.checkPassword(payload.imagePassword());
                }
 
                checkArgument(
